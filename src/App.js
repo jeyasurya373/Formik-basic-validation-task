4e22f19b1f172a13ba87a8be2c042be97f0b1fa8
Jeya surya <jeyasurya@mavencart.com>
2023-03-19 16:13:19 +0530

    formik basic task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from "formik";
 
+const initialValues ={
+      name:'',
+      email:'',
+      hobbie:''
+};
+
+const onSubmit = value =>{
+  console.log(value)
+};
+
+const validate = value =>{
+  let errors ={}
+
+  if(!value.name){
+    errors.name ='Required'
+  }
+  if(!value.email){
+    errors.email ='Required'
+  }
+  if(!value.hobbie){
+    errors.hobbie = 'Required'
+  }
+
+  return errors
+
+}
 function App() {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate
+  })
+  console.log(formik.errors)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form style={{display:'flex',flexDirection:"column",width:"50%"}} onSubmit={formik.handleSubmit}>
+
+        <label htmlFor="name">Name</label>
+        <input type='text'
+         id="name"
+        name="name" 
+        onChange={formik.handleChange} 
+        onBlur={formik.handleBlur} 
+        value={formik.values.name}
+        />
+        {formik.touched.name && formik.errors.name ? <div style={{color:'red'}}>{formik.errors.name}</div>:''}
+
+        <label htmlFor="email">Email</label>
+        <input type='email' 
+        id="email" 
+        name="email" 
+        onChange={formik.handleChange} 
+        onBlur={formik.handleBlur} 
+        value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? <div style={{color:'red'}}>{formik.errors.email}</div>:''}
+
+        <label htmlFor="hobbie">Hobbie</label>
+        <input type='text' 
+        id="hobbie" 
+        name="hobbie" 
+        onChange={formik.handleChange} 
+        onBlur={formik.handleBlur} 
+        value={formik.values.hobbie}
+        />
+        {formik.touched.hobbie && formik.errors.hobbie ? <div style={{color:'red'}}>{formik.errors.hobbie}</div>:''}
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
